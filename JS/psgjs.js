de95ccc6485551daa109811417c9ae6cccd1e3db
tomnmillie <tjaffarian@gmail.com>
2021-05-25 22:23:53 -0400

    Delete JS directory

--- JS/psgjs.js
+++
@@ -1,8 +0,0 @@
-function myNavCollapse() {
-  var x = document.getElementById("psgTopnav");
-  if (x.className === "psgtopnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "psgtopnav";
-  }
-}
